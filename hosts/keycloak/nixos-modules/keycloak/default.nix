1d88abe492e733c2f5a76b4e7e0de148e62d7980
Forestcat <forest_cat@dismail.de>
2024-05-25 13:52:31 +0200

    trying to fix keycloak

--- hosts/keycloak/nixos-modules/keycloak/default.nix
+++ hosts/keycloak/nixos-modules/keycloak/default.nix
@@ -12,10 +12,11 @@
     settings = {
       hostname = "id.forestcat.org";
       hostname-strict-backchannel = false;
+      proxy = "edge";
     };
     initialAdminPassword = "changeme";  # change on first login
-    sslCertificate = "/home/nixuser/keys/forestcat.org_ssl_certificate.cer";
-    sslCertificateKey = "/home/nixuser/keys/forestcat.org_private_key.key";
+    # sslCertificate = "/home/nixuser/keys/forestcat.org_ssl_certificate.cer";
+    # sslCertificateKey = "/home/nixuser/keys/forestcat.org_private_key.key";
     database.createLocally = true;
     database.username = "keycloak";
     database.passwordFile = "/home/nixuser/keys/keycloak-keyfile";
