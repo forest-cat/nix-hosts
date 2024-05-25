b1ba57a1f83d5f70656782c94083b3bebf758d50
Forestcat <71219154+forest-cat@users.noreply.github.com>
2024-05-25 03:58:30 +0200

    update ssh config

--- common/home-modules/ssh/default.nix
+++ common/home-modules/ssh/default.nix
@@ -3,29 +3,10 @@
 {
   programs.ssh = {
     extraConfig = "
-          Host mxserver
-            HostName iciclefurry.de
-            User minecraft
-            Port 106  
-            IdentityFile ~/.ssh/ssh_key
-          Host mc-create
-            HostName mc-create.fritz.box
-            User xuser
-            Port 22
-            IdentityFile ~/.ssh/ssh_key
-          Host jxserver
-            HostName iciclefurry.de
-            User josua
-            Port 106
-            IdentityFile ~/.ssh/ssh_key
-          Host fnuc
-            HostName nuc.fritz.box
-            User forestcat
-            Port 22
           Host github
             HostName github.com
             User git
-            IdentityFile ~/.ssh/git_key";
+            IdentityFile ~/.ssh/github_readonly_deploykey";
     enable = true;
 
   };
