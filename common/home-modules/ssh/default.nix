e37b384887ce24b41641935a28af92057ffc146f
Forestcat <71219154+forest-cat@users.noreply.github.com>
2024-05-25 04:20:20 +0200

    temp fix for ssh config

--- common/home-modules/ssh/default.nix
+++ common/home-modules/ssh/default.nix
@@ -3,10 +3,6 @@
 {
   programs.ssh = {
     extraConfig = "
-#          Host github
-#            HostName github.com
-#            User git
-#            IdentityFile ~/.ssh/github_readonly_deploykey";
           Host github
             HostName github.com
             User git
