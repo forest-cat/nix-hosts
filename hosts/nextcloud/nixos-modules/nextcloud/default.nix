6e05b19e63585ac6827529a54c2b2e572ce5ac22
Forestcat <forest_cat@dismail.de>
2024-05-29 12:02:23 +0200

    update nextcloud to version 27

--- hosts/nextcloud/nixos-modules/nextcloud/default.nix
+++ hosts/nextcloud/nixos-modules/nextcloud/default.nix
@@ -8,6 +8,7 @@
 {
   services.nextcloud = {
     enable = true;
+    package = pkgs.nextcloud27;
     hostName = "nextcloud.forestcat.org";
     autoUpdateApps.enable = true;
     database.createLocally = true;
