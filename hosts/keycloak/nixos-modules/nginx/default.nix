961499fc42a3d58839ec93d2bef40ddf5c27169c
Forestcat <forest_cat@dismail.de>
2024-06-05 21:22:42 +0200

    switch realms to master

--- hosts/keycloak/nixos-modules/nginx/default.nix
+++ hosts/keycloak/nixos-modules/nginx/default.nix
@@ -13,7 +13,7 @@
         locations = {
           # Redirect a user opening any not set location on id.forestcat.org to the account management page.
           "^~ /" = {
-            return = "307 https://id.forestcat.org/realms/nekoverse/account/";
+            return = "307 https://id.forestcat.org/realms/master/account/";
           };
           "/js/" = {
             proxyPass = "http://127.0.0.1:8080/js/";
