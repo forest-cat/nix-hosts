bbcd558c5d680a17e7d0d9606992b7c99c7c1d10
Forestcat <forest_cat@dismail.de>
2024-10-20 12:53:46 +0200

    add mail for firefly

--- hosts/firefly/nixos-modules/firefly/default.nix
+++ hosts/firefly/nixos-modules/firefly/default.nix
@@ -19,6 +19,14 @@
       APP_KEY_FILE = "/var/secrets/firefly-iii-app-key.txt";
       DB_CONNECTION = "sqlite";
       TRUSTED_PROXIES="**";
+      MAIL_MAILER="smtp";
+      MAIL_HOST="smtp.migadu.com";
+      MAIL_PORT=465;
+      MAIL_FROM="firefly@forestcat.org";
+      MAIL_USERNAME="firefly@forestcat.org";
+      MAIL_PASSWORD_FILE="/var/secrets/firefly-iii-smtp-password.txt";
+      MAIL_ENCRYPTION="tls";
+
     };
   };
 }
