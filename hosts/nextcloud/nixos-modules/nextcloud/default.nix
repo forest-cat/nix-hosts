446b015a66efc04bae8153db5721936061513cfd
forest-cat <github@forestcat.org>
2026-02-09 23:01:34 +0100

    update nextcloud major version 31 -> 32

--- hosts/nextcloud/nixos-modules/nextcloud/default.nix
+++ hosts/nextcloud/nixos-modules/nextcloud/default.nix
@@ -8,7 +8,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud32;
     hostName = "cloud.forestcat.org";
     autoUpdateApps.enable = true;
     database.createLocally = true;
