5cc61d4aaafe90b725f9dc8bcf6aac15adda3a8a
forest-cat <github@forestcat.org>
2026-02-09 23:08:05 +0100

    set ssh default options due to them being removed by default

--- common/home-modules/ssh/default.nix
+++ common/home-modules/ssh/default.nix
@@ -3,6 +3,18 @@
 {
   programs.ssh = {
     enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
     extraConfig = "
           Host github
             HostName github.com
