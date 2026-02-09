4f14e80b005a29dbbde4479f8cdb011daa66440e
forest-cat <github@forestcat.org>
2026-02-09 22:36:20 +0100

    update stable to 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Nixos Flake for my selfhosted services";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
